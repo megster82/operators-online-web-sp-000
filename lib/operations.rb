9862a2da4edade276fc95d9c65f43ca4ff9031fb
meaghan jennings <meaghanljennings@gmail.com>
2020-07-15 17:00:09 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-
+  if speed.between?(40,60) == true
+    return false 
+    elsif speed < 40 || speed > 60 == true
+    return true
+  else
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
-	
-
-
